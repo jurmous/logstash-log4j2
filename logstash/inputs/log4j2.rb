d01454833cadafe0044a10cb9562f5d7531285f6
Jurriaan Mous <jurmous@jurmo.us>
2014-06-30 12:32:37 +0200

    Update the log4j2 plugin to use Log4j2 native  org.apache.logging.log4j.core.impl.Log4jLogEvent
    Bug in Log4jLogEvent stacktrace was fixed in RC2 of log4J2
    https://issues.apache.org/jira/browse/LOG4J2-542

--- logstash/inputs/log4j2.rb
+++ logstash/inputs/log4j2.rb
@@ -1,22 +1,18 @@
 # encoding: utf-8
 require "logstash/inputs/base"
+require "logstash/errors"
+require "logstash/environment"
 require "logstash/namespace"
 require "logstash/util/socket_peer"
 require "socket"
 require "timeout"
 
-# Read events over a TCP socket from a Log4j2 SocketAppender with the 
-# SerializedLogEventLayout
-#
-# This Layout is provided by the included jar or on:
-# 
+# Read events over a TCP socket from a Log4j2 SocketAppender.
 #
 # Can either accept connections from clients or connect to a server,
 # depending on `mode`. Depending on which `mode` is configured,
 # you need a matching SocketAppender or a SocketHubAppender
 # on the remote side.
-# 
-# 
 class LogStash::Inputs::Log4j2 < LogStash::Inputs::Base
 
   config_name "log4j2"
@@ -49,42 +45,49 @@ class LogStash::Inputs::Log4j2 < LogStash::Inputs::Base
     require "java"
     require "jruby/serialization"
 
-    if __FILE__ !~ /^(jar:)?file:\/\//
-      if File.exists?(File.dirname(__FILE__)+"/Log4jExtension-0.3.0.jar")
-        require File.dirname(__FILE__)+"/Log4jExtension-0.3.0.jar"
-      end
+    begin
+			if __FILE__ !~ /^(jar:)?file:\/\//
+		  	if File.exists?(File.dirname(__FILE__)+"/log4j-api-2.0-rc2.jar")
+ 					require File.dirname(__FILE__)+"/log4j-api-2.0-rc2.jar"
+ 		  	end
+		  	if File.exists?(File.dirname(__FILE__)+"/log4j-core-2.0-rc2.jar")
+					require File.dirname(__FILE__)+"/log4j-core-2.0-rc2.jar"
+		  	end
+		  end
+      Java::OrgApacheLoggingLog4jCoreImpl.const_get("Log4jLogEvent")
+    rescue
+       raise(LogStash::PluginLoadingError, "Log4j2 java library not loaded")
     end
 
     if server?
       @logger.info("Starting Log4j2 input listener", :address => "#{@host}:#{@port}")
       @server_socket = TCPServer.new(@host, @port)
     end
-    @logger.info("Log4j2 input")
+    @logger.info("Log4j input")
   end # def register
 
   private
   def handle_socket(socket, output_queue)
-    begin
+    begin   	
+    
       # JRubyObjectInputStream uses JRuby class path to find the class to de-serialize to
-	   ois = JRubyObjectInputStream.new(java.io.BufferedInputStream.new(socket.to_inputstream))
-      
+      ois = JRubyObjectInputStream.new(java.io.BufferedInputStream.new(socket.to_inputstream))
       loop do
-        # NOTE: event_raw is jurmous.log4j.SimpleLogEvent
-   		log4j_obj = ois.readObject
-        event = LogStash::Event.new("message" => log4j_obj.message, LogStash::Event::TIMESTAMP => Time.at(log4j_obj.timestamp/1000,log4j_obj.timestamp%1000*1000).gmtime)
+        # NOTE: log4j_obj is org.apache.logging.log4j.core.impl.Log4jLogEvent
+        log4j_obj = ois.readObject
+        event = LogStash::Event.new("message" => log4j_obj.getMessage.getFormattedMessage, LogStash::Event::TIMESTAMP => Time.at(log4j_obj.getTimeMillis/1000,log4j_obj.getTimeMillis%1000*1000).gmtime)
         decorate(event)
         event["host"] = socket.peer
-		event["marker"] = log4j_obj.marker if log4j_obj.marker
-        event["priority"] = log4j_obj.level
-        event["logger_name"] = log4j_obj.name
-        event["thread"] = log4j_obj.threadName 
-        event["class"] = log4j_obj.location.getClassName
-        event["file"] = log4j_obj.location.getFileName + ":" + log4j_obj.location.getLineNumber.to_s
-        event["method"] = log4j_obj.location.getMethodName
-        
-        event["thrown"] = log4j_obj.throwableName if log4j_obj.throwableName
-        event["stack_trace"] = log4j_obj.throwableStack if log4j_obj.throwableStack
-        
+        event["marker"] = log4j_obj.getMarker.getName if log4j_obj.getMarker
+        event["path"] = log4j_obj.getLoggerName
+        event["priority"] = log4j_obj.getLevel.toString
+        event["logger_name"] = log4j_obj.getLoggerName
+        event["thread"] = log4j_obj.getThreadName
+        event["class"] = log4j_obj.getSource().getClassName
+        event["file"] = log4j_obj.getSource().getFileName + ":" + log4j_obj.getSource().getLineNumber.to_s
+        event["method"] = log4j_obj.getSource().getMethodName
+        event["stack_trace"] = log4j_obj.getThrownProxy.getExtendedStackTrace.to_a.join("\n") if log4j_obj.getThrownProxy
+
         # Add the context properties to '@fields'
         if log4j_obj.contextMap
           log4j_obj.contextMap.keySet.each do |key|
@@ -92,12 +95,12 @@ class LogStash::Inputs::Log4j2 < LogStash::Inputs::Base
           end  
         end
         
-		event["cstack"] = log4j_obj.contextStack.to_a if log4j_obj.contextStack  
+        event["cstack"] = log4j_obj.getContextStack.to_a if log4j_obj.getContextStack  
 
         output_queue << event
       end # loop do
     rescue => e
-    @logger.debug(e)
+    	@logger.debug(e)
       @logger.debug("Closing connection", :client => socket.peer,
                     :exception => e)
     rescue Timeout::Error
